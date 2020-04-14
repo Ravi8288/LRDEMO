2c4f6c482007db40173d6b30b7ce4944315c9cdb
Ravi8288 <63417439+Ravi8288@users.noreply.github.com>
2020-04-14 11:33:35 -0400

    Update Action.c

--- Action.c
+++ Action.c
@@ -2,7 +2,7 @@ Action()
 {
 	//This is my first pull and commit
 	web_custom_request("web_custom_request",
-		"URL=www.google.com",
+		"URL=www.flipkart.com",
 		"Method=GET",
 		"TargetFrame=",
 		"Resource=0",
