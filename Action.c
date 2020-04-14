08544f2ba195049b0efaf08fbbf5374277f1e76a
Ravi8288 <63417439+Ravi8288@users.noreply.github.com>
2020-04-14 11:32:18 -0400

    Update Action.c

--- Action.c
+++ Action.c
@@ -2,7 +2,7 @@ Action()
 {
 	//This is my first pull and commit
 	web_custom_request("web_custom_request",
-		"URL=www.mintra.com",
+		"URL=www.google.com",
 		"Method=GET",
 		"TargetFrame=",
 		"Resource=0",
